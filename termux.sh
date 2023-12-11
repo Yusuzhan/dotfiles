778880ffc641137748f8a68fa47cba41663c2e04
Suzhan Yu <suzhan.yu@partner.bmw.com>
2023-12-11 15:32:52 +0800

    termux.sh: added neovim build script

--- termux.sh
+++ termux.sh
@@ -1,1 +1,9 @@
-pkg install openssl cmake gmake unzip wget gettext curl
+# install neovim prerequisites
+pkg install openssl cmake unzip wget gettext curl
+
+# clone neovim repo
+git clone https://github.com/neovim/neovim $HOME/dev/neovim
+cd $HOME/dev/neovim
+git checkout stable
+make CMAKE_BUILD=Release
+make install
