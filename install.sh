fe5186382f4e603f78d43c14067d99cbb77ecbee
Suzhan Yu <yusuzhan@msn.com>
2026-02-28 19:56:41 +0800

    install.sh: reformat

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 os="Ubuntu"
 unameOut="$(uname -a)"
 if [[ "$unameOut" == *"Linux"* ]]; then
-				osRelease="$(cat /etc/os-release)"
-				if [[ "$osRelease" == *"Arch"* ]]; then
-								os="Arch"
-				elif [[ "$osRelease" == *"Ubuntu"* ]]; then
-								os="Ubuntu"
-				fi
+  osRelease="$(cat /etc/os-release)"
+  if [[ "$osRelease" == *"Arch"* ]]; then
+    os="Arch"
+  elif [[ "$osRelease" == *"Ubuntu"* ]]; then
+    os="Ubuntu"
+  fi
 elif [[ "$unameOut" == *"Darwin"* ]]; then
-				os="macos"
+  os="macos"
 fi
 
 echo "OS: $os"
@@ -23,39 +23,37 @@ if [[ $OS == "macOS" ]]; then
   defaults write com.google.android.studio ApplePressAndHoldEnabled -bool false
 fi
 
-
-
 # install oh-my-zsh
 if [ -d $HOME/.oh-my-zsh ]; then
-    echo '[oh-my-zsh] already installed'
+  echo '[oh-my-zsh] already installed'
 else
-    echo '[oh-my-zsh] start installing...'
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo '[oh-my-zsh] start installing...'
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # install zsh-auto-suggestions
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
-    echo '[zsh-autosuggestions] already installed'
+  echo '[zsh-autosuggestions] already installed'
 else
-    echo '[zsh-autosuggestions] start installing'
-		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  echo '[zsh-autosuggestions] start installing'
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 # install zsh-syntax-highlighting
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-    echo '[zsh-syntax-highlighting] already installed'
+  echo '[zsh-syntax-highlighting] already installed'
 else
-    echo '[zsh-syntax-highlighting] start installing'
-		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  echo '[zsh-syntax-highlighting] start installing'
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 # link neovim settings
 echo "[neovim] update config"
 if ! [ -d $CONFIG_DIR ]; then
-    mkdir $CONFIG_DIR
+  mkdir $CONFIG_DIR
 fi
- 
-ln -sf $DOTFILES_DIR/nvim $CONFIG_DIR/nvim 
+
+ln -sf $DOTFILES_DIR/nvim $CONFIG_DIR/nvim
 
 # install powerlevel10k theme
 #if ! [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
@@ -68,9 +66,9 @@ ln -sf $DOTFILES_DIR/nvim $CONFIG_DIR/nvim
 # link .zshrc
 echo "[zshrc] update config "
 if [[ $OS == "macOS" ]]; then
-    ln -sf $DOTFILES_DIR/zshrc/.zshrc.macos $HOME/.zshrc
+  ln -sf $DOTFILES_DIR/zshrc/.zshrc.macos $HOME/.zshrc
 else
-    ln -sf $DOTFILES_DIR/zshrc/.zshrc.linux $HOME/.zshrc
+  ln -sf $DOTFILES_DIR/zshrc/.zshrc.linux $HOME/.zshrc
 fi
 
 # link alacritty config
@@ -96,4 +94,3 @@ tmux source $HOME/.tmux.conf
 # echo "[conky] update config "
 # mkdir -p $CONFIG_DIR/conky
 # ln -sf $DOTFILES_DIR/conky/conky.conf $CONFIG_DIR/conky/conky.conf
-
